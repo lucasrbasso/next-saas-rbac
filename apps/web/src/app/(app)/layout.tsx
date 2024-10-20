dea89acf4042663a889155967876e4d0cce67a91
lucasrbasso <lucas.rbasso@outlook.com>
2024-10-20 16:09:00 -0300

    feat: using parallel routing and interception routes

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,12 +1,14 @@
 import { redirect } from 'next/navigation'
+import React from 'react'
 
 import { isAuthenticated } from '@/auth/auth'
-import { Header } from '@/components/header'
 
 export default function AppLayout({
   children,
+  sheet,
 }: Readonly<{
   children: React.ReactNode
+  sheet: React.ReactNode
 }>) {
   if (!isAuthenticated()) {
     redirect('/auth/sign-in')
@@ -14,8 +16,10 @@ export default function AppLayout({
 
   return (
     <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px]">{children}</main>
+      <main className="mx-auto w-full max-w-[1200px]">
+        {children}
+        {sheet}
+      </main>
     </div>
   )
 }
